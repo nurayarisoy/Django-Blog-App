4cff2b109607f6646867694fb4f17e1fd9ddc3d3
SUAT ARISOY <cevriyeceven@gmail.com>
2022-02-17 12:16:47 +0100

    feat:add settings

--- main/settings.py
+++ main/settings.py
@@ -133,8 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIR = (
  os.path.join(BASE_DIR, 'static'),
 )
 
